5cc3dd4a4203b5dd45c5f6d576b8022574f82415
João Gallo <joaogallohd@gmail.com>
2026-01-14 13:43:11 -0300

    feat: removed all .css files on projects and implementad de styled-components on index.js and app.js

--- src/components/optionsHeader/index.js
+++ src/components/optionsHeader/index.js
@@ -1,16 +1,31 @@
-import './style.css';
+import styled from 'styled-components';
+
+const OptionsContainer = styled.ul`
+  display: flex;
+`;
+
+const OptionContainer = styled.li`
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+  min-width: 120px;
+`;
 
 const textOptions = ['CATEGORIES', 'FAVORITES', 'MY WISH LIST'];
 
 function OptionsHeader() {
   return (
-    <ul className="options">
+    <OptionsContainer className="options">
       {textOptions.map((text) => (
-        <li className="option" key={text}>
+        <OptionContainer className="option" key={text}>
           <p>{text}</p>
-        </li>
+        </OptionContainer>
       ))}
-    </ul>
+    </OptionsContainer>
   );
 }
 
