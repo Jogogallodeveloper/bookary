de6d0221a7e19a83559cc07b3ac8e375ba47debf
João Gallo <joaogallohd@gmail.com>
2026-01-15 23:44:29 -0300

    feat: implementation of states on project

---
+++ src/components/search/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function Search() {
+  return (
+    <section>
+      <h2>Not sure where to start ?</h2>
+      <h3>Find here your next read </h3>
+      <input placeholder="Pleas type hear your next book" />
+    </section>
+  );
+}
+
+export default Search;
